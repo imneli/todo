8342929914ef68b357880c77e6fa6fa94b3e7dcf
imneli <contatodoneli@gmail.com>
2024-06-30 21:40:39 -0300

    app.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,6 @@ import React from 'react';
 import Main from './components/Main'
 import './App.css';
 
-// function App() {
-//   return (
-//     <div>
-//       <h1>Olá mundo</h1>
-//       <p>asdasd asdsadas d</p>
-//     </div>
-//   );
-// }
-
 function App(){
   return <Main />
 }
